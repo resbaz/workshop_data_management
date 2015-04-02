b08d8502e0006b0d6b4b5af24a1dd3b0cc05baf6
Damien Irving <damien_irving@yahoo.com.au>
2015-04-03 10:00:18 +1100

    The server runs properly

--- workshops/admin.py
+++ workshops/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from workshops.models import Applicant, Helper, Instructor, Organisation, Person, Student, Workshop
+
+admin.site.register(Applicant)
+admin.site.register(Helper)
+admin.site.register(Instructor)
+admin.site.register(Organisation)
+admin.site.register(Person)
+admin.site.register(Student)
+admin.site.register(Workshop)
--- workshops/models.py
+++ workshops/models.py
@@ -6,88 +6,24 @@
 from django.db import models
 
 
-class Person(models.Model):
-    """The underlying model for a person."""
-
-    name = models.CharField(max_length=128)
-    email = models.EmailField()
-    gender = models.CharField(u'gender', max_length=30, choices=GENDER_CHOICES, default=u'en')
-    dob = models.DateField(u'Date of Birth', blank=True, null=True)    
-
-
-class Organisation(models.Model):
-     """The underlying model for organisations."""
-
-    institution = models.CharField(u'institution', max_length=200, choices=ORG_CHOICES, default=u'en')
-    campus = models.CharField(u'campus', max_length=200, choices=**DEPENDANT CHOICES**, default=u'en')
-    department = models.CharField(u'department', max_length=200, choices=**DEPENDANT CHOICES**, default=u'en')
-
-
-class Workshop(models.Model):
-    """The underlying model for workshops."""
-
-    title = models.CharField(u'title', max_length=100)
-    description = models.CharField( body text )
-    date = models.DateField(u'Date of Birth' null=True)
-    teaching_hours = models.IntegerField()
-    catering = models.BooleanField()
-
-    instructors = models.ManyToManyField(Person, through='Instructor')
-    helpers = models.ManyToManyField(Person, through='Helper')
-    student = models.ManyToManyField(Person, through='Student')
-    applicant = models.ManyToManyField(Person, through='Applicant')
-
-    class Meta:
-        ordering = ['date',]
-
-
-class Instructor(models.Model):
-    """Intermediate class for instructors."""
-
-    workshop = models.ForeignKey(Workshop)
-    person = models.ForeignKey(Person)
-    organisation = models.ForeignKey(Organisation)
-    career = models.CharField(u'career stage', max_length=30, choices=CAREER_CHOICES, default=u'en')
-    diet = models.CharField(u'dietary requirements', max_length=30, choices=DIET_CHOICES, default=u'en')
-
-
-class Helper(models.Model):
-    """Intermediate class for helpers."""
-
-    workshop = models.ForeignKey(Workshop)
-    person = models.ForeignKey(Person)
-    organisation = models.ForeignKey(Organisation)
-    career = models.CharField(u'career stage', max_length=30, choices=CAREER_CHOICES, default=u'en')
-    diet = models.CharField(u'dietary requirements', max_length=30, choices=DIET_CHOICES, default=u'en')
-    attendance = models.BooleanField()
-
-
-class Student(models.Model):
-    """Intermediate class for students."""
-
-    workshop = models.ForeignKey(Workshop)
-    person = models.ForeignKey(Person)
-    organisation = models.ForeignKey(Organisation)
-    career = models.CharField(u'career stage', max_length=30, choices=CAREER_CHOICES, default=u'en')
-    diet = models.CharField(u'dietary requirements', max_length=30, choices=DIET_CHOICES, default=u'en')
-    attendance = models.BooleanField()
-
-
-class Applicants(models.Model):
-    """Intermediate class for applicants."""
-
-    workshop = models.ForeignKey(Workshop)
-    person = models.ForeignKey(Person)
-    organisation = models.ForeignKey(Organisation)
-    career = models.CharField(u'career stage', max_length=30, choices=CAREER_CHOICES, default=u'en')
-    diet = models.CharField(u'dietary requirements', max_length=30, choices=DIET_CHOICES, default=u'en')
-
-
-
 GENDER_CHOICES = (
     ('m', 'Male'),
     ('f', 'Female'),
     ('o', 'Other'),
+    )
+
+CAREER_CHOICES = (
+    (1,'phd'),
+    (2, 'ecr'),
+    (3, 'postdoc'),
+    )
+
+DIETARY_CHOICES = (
+    (1, 'vegetarian'),
+    (2, 'vegan'),
+    (3, 'gluten free'),
+    (5, 'lactose intolerant'),
+    (6, 'halal'),
     )
 
 ORG_CHOICES = (
@@ -102,56 +38,47 @@ ORG_CHOICES = (
     (9,'Bionics Institute'),
     (10,'Bureau of Meteorology'),
     (11,'Burnet Institute'),
-    (12,'Charles Sturt University (Albury-Wodonga)'),
-    (13,'Charles Sturt University (Wagga Wagga)'),
-    (14,'CSIRO (Clayton)'),
-    (15,'CSIRO (Hobart)'),
+    (12,'Charles Sturt University'),
+    (14,'CSIRO'),
     (16,'Curtin University of Technology'),
-    (17,'Deakin University (Burwood)'),
-    (18,'Deakin University (Warrnambool)'),
+    (18,'Deakin University'),
     (19,'Edith Cowan University'),
-    (20,'Federation University (Ballarat)'),
+    (20,'Federation University'),
     (21,'Fred Hollows Foundation'),
     (22,'Griffith University'),
-    (23,'Griffith University (Gold Coast)'),
     (24,'IBM Research'),
     (25,'James Cook University'),
-    (26,'La Trobe University (Bendigo)'),
-    (27,'La Trobe University (Bundoora)'),
+    (26,'La Trobe University'),
     (28,'Macquarie University'),
     (29,'Monash Alfred Psychiatry Research Centre'),
-    (30,'Monash University (Clayton)'),
-    (31,'Monash University (Parkville)'),
+    (30,'Monash University'),
     (32,'Murdoch Childrens Research Institute'),
     (33,'Museum Victoria'),
     (34,'New Zealand eScience Infrastructure'),
     (35,'Peter MacCallum Cancer Centre'),
-    (36,'Queensland Government (Department of Agriculture, Fisheries an Forestry)'),
+    (36,'Queensland Government'),
     (37,'Queensland University of Technology'),
     (38,'RMIT'),
     (39,'Software Sustainability Institute'),
-    (40,'Southern Cross University (Coffs Harbour)'),
-    (41,'Swinburne University of Technology (Hawthorn)'),
+    (40,'Southern Cross University'),
+    (41,'Swinburne University of Technology'),
     (42,'University of Adelaide'),
     (43,'University of Auckland'),
     (44,'University of Canterbury'),
-    (45,'University of Melbourne (Burnley)'),
-    (46,'University of Melbourne (Parkville)'),
+    (45,'University of Melbourne'),
     (47,'University of New England'),
     (48,'University of Otago'),
     (49,'University of Queensland'),
     (50,'University of Southern Queensland'),
     (51,'University of Sydney'),
-    (52,'University of Tasmania (Cradle Coast)'),
-    (53,'University of Tasmania (Hobart)'),
-    (54,'University of Technology, Sydney'),
+    (52,'University of Tasmania'),
+    (54,'University of Technology Sydney'),
     (55,'University of the Sunshine Coast'),
     (56,'University of Western Australia'),
     (57,'University of Western Sydney'),
     (58,'University of Wollongong'),
-    (59,'UNSW (Canberra)'),
-    (60,'UNSW (Sydney)'),
-    (61,'Victoria University (Footscray)'),
+    (59,'UNSW'),
+    (61,'Victoria University'),
     (62,'Walter and Eliza Hall Institute of Medical Research'),
     )
 
@@ -194,7 +121,7 @@ DEPT_CHOICES = (
     (36,'Medical Education Unit'),
     (37,'Department of Medicine at Austin Health'),
     (38,'Department of Medicine at Royal Melbourne Hospital'),
-    (39,'Department of Medicine at St Vincent's'),
+    (39,'Department of Medicine at St Vincents'),
     (40,'Department of Microbiology and Immunology'),
     (41,'NorthWest Academic Centre'),
     (42,'Department of Obstetrics and Gynaecology'),
@@ -209,7 +136,7 @@ DEPT_CHOICES = (
     (51,'Rural Health Academic Centre'),
     (52,'Department of Surgery at Austin Health'),
     (53,'Department of Surgery at Royal Melbourne Hospital'),
-    (54,'Department of Surgery at St Vincent’s'),
+    (54,'Department of Surgery at St Vincents'),
     (55,'Melbourne School of Health Sciences'),
     (56,'Department of Nursing'),
     (57,'Department of Physiotherapy'),
@@ -237,4 +164,79 @@ DEPT_CHOICES = (
     (79,'Scholarly Information'),
     (80,'VLSCI'),
     )
+
+
+class Person(models.Model):
+    """The underlying model for a person."""
+
+    name = models.CharField(max_length=128)
+    email = models.EmailField()
+    gender = models.CharField(u'gender', max_length=2, choices=GENDER_CHOICES, default='o')
+    dob = models.DateField(u'Date of Birth', blank=True, null=True)    
+
+
+class Organisation(models.Model):
+    """The underlying model for organisations."""
+
+    institution = models.CharField(u'institution', max_length=200, choices=ORG_CHOICES)
+    campus = models.CharField(u'campus', max_length=200)
+    department = models.CharField(u'department', max_length=200)
+
+
+class Workshop(models.Model):
+    """The underlying model for workshops."""
+
+    title = models.CharField(u'title', max_length=100)
+    description = models.CharField(max_length=200)
+    start_date = models.DateField()
+    teaching_hours = models.IntegerField()
+    catering = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['start_date',]
+
+
+class Instructor(models.Model):
+    """Intermediate class for instructors."""
+
+    workshop = models.ForeignKey(Workshop)
+    person = models.ForeignKey(Person)
+    organisation = models.ForeignKey(Organisation)
+    career_stage = models.CharField(u'career stage', max_length=4, choices=CAREER_CHOICES)
+    dietary_requirements = models.CharField(u'dietary requirements', max_length=2, choices=DIETARY_CHOICES)
+
+
+class Helper(models.Model):
+    """Intermediate class for helpers."""
+
+    workshop = models.ForeignKey(Workshop)
+    person = models.ForeignKey(Person)
+    organisation = models.ForeignKey(Organisation)
+    career_stage = models.CharField(u'career stage', max_length=4, choices=CAREER_CHOICES)
+    dietary_requirements = models.CharField(u'dietary requirements', max_length=2, choices=DIETARY_CHOICES)
+    attendance = models.BooleanField(default=False)
+
+
+class Student(models.Model):
+    """Intermediate class for students."""
+
+    workshop = models.ForeignKey(Workshop)
+    person = models.ForeignKey(Person)
+    organisation = models.ForeignKey(Organisation)
+    career_stage = models.CharField(u'career stage', max_length=4, choices=CAREER_CHOICES)
+    dietary_requirements = models.CharField(u'dietary requirements', max_length=2, choices=DIETARY_CHOICES)
+    attendance = models.BooleanField(default=False)
+
+
+class Applicant(models.Model):
+    """Intermediate class for applicants."""
+
+    workshop = models.ForeignKey(Workshop)
+    person = models.ForeignKey(Person)
+    organisation = models.ForeignKey(Organisation)
+    career_stage = models.CharField(u'career stage', max_length=4, choices=CAREER_CHOICES)
+    dietary_requirements = models.CharField(u'dietary requirements', max_length=2, choices=DIETARY_CHOICES)
+
+
+
 
