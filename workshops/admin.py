4b310ecf6dca5f40c4c01d505d40670d4c4fd16d
Damien Irving <damien_irving@yahoo.com.au>
2015-04-03 13:43:46 +1100

    Added filters

--- workshops/admin.py
+++ workshops/admin.py
@@ -4,12 +4,16 @@ from workshops.models import Person, Workshop, Participant
 
 class PersonAdmin(admin.ModelAdmin):
     list_display = ['__unicode__', 'notes']
+    list_filter = ['teaching_teams']
 
 class WorkshopAdmin(admin.ModelAdmin):
     list_display = ['__unicode__', 'start_date', 'description']
 
 class ParticipantAdmin(admin.ModelAdmin):
     list_display = ['workshop', 'person', 'role']
+    list_filter = ['person', 'workshop', 'role',
+                   'dietary_requirements', 'attendance']
+
 
 admin.site.register(Person, PersonAdmin)
 admin.site.register(Workshop, WorkshopAdmin)
