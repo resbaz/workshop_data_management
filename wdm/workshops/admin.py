af4557e9347c239591d3b2ada89ac07f14580022
Damien Irving <irving.damien@gmail.com>
2015-04-17 04:20:33 +0000

    Added beginnings of import_export code

--- wdm/workshops/admin.py
+++ wdm/workshops/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
+#from import_export import resources
+#from import_export.admin import ImportExportActionModelAdmin
 
 from workshops.models import Person, Workshop, Participant, Institution
+
+#class WorkshopResource(resources.ModelResource):
+    
+#    class Meta:
+#        model Workshop
+
 
 class PersonAdmin(admin.ModelAdmin):
     list_display = ['__unicode__', 'notes']
@@ -8,6 +16,7 @@ class PersonAdmin(admin.ModelAdmin):
 
 class WorkshopAdmin(admin.ModelAdmin):
     list_display = ['__unicode__', 'start_date', 'description']
+    #resource_class = WorkshopResource    
 
 class ParticipantAdmin(admin.ModelAdmin):
     list_display = ['workshop', 'person', 'role']
@@ -27,3 +36,4 @@ admin.site.register(Person, PersonAdmin)
 admin.site.register(Workshop, WorkshopAdmin)
 admin.site.register(Participant, ParticipantAdmin)
 admin.site.register(Institution, InstitutionAdmin)
+
