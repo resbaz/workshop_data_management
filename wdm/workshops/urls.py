e25cbf5a842758918b255d33626cede85a9b9c96
Lachlan Musicman <datakid@gmail.com>
2015-04-24 14:33:48 +1000

    found the damn url routing bug

--- wdm/workshops/urls.py
+++ wdm/workshops/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url, patterns
 from .views import WorkshopList, WorkshopDetail, WorkshopCreate, WorkshopUpdate, WorkshopDelete
 
 urlpatterns = patterns('',
-    url(r'^', WorkshopList.as_view(), name='index'),
+    url(r'^$', WorkshopList.as_view(), name='index'),
     url(r'^workshops/$', WorkshopList.as_view(), name='index'),
     url(r'^workshops/add/$', WorkshopCreate.as_view(), name='workshop_add'),
     url(r'^workshops/(?P<slug>[-_\w]+)/$', WorkshopDetail.as_view(), name='workshop_detail'),
