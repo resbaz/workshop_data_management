23c7767911e4dec45bd45247ebf62e2efe16bfe7
Damien Irving <irving.damien@gmail.com>
2015-04-24 00:29:42 +0000

    added the uwsgi file for reference, made adjustments for launch

--- wdm/workshop_data_management/wsgi.py
+++ wdm/workshop_data_management/wsgi.py
@@ -10,5 +10,5 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "workshop_data_management.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "workshop_data_management.settings.local")
 application = get_wsgi_application()
